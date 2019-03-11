bde3640813a0a8450faa2f56ca846ee5f4735f1e
murat <muratkarsh@gmail.com>
2019-03-11 18:47:49 +0200

    add random planet toggler

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './app.scss'
 import Header from '../header'
 import RandomPlanet from '../random-planet'
 import ItemList from "../item-list"
 import PersonDetails from "../person-details"
 
-const App = () => {
-    return (
-        <div className='app'>
-            <div className="container">
-                <Header />
+export default class App extends Component {
+    state = {
+        showRandomPlanet: true
+    }
 
-                <div className="row">
-                    <div className="col col-xs-12">
-                        <RandomPlanet />
+    toggleRandomPlanet = () => {
+        this.setState(state => {
+            return {
+                showRandomPlanet: !state.showRandomPlanet
+            }
+        })
+    }
+
+    render() {
+        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null
+
+        return (
+            <div className='app'>
+                <div className="container">
+                    <Header />
+
+                    <div className="row">
+                        <button
+                            onClick={this.toggleRandomPlanet}
+                        >
+                            Toggle random planet
+                        </button>
                     </div>
-                </div>
 
-                <div className="row">
-                    <div className="col col-md-12 col-lg-6">
-                        <ItemList />
+                    <div className="row">
+                        <div className="col col-xs-12">
+                            {planet}
+                        </div>
                     </div>
 
-                    <div className="col col-md-12 col-lg-6">
-                        <PersonDetails />
+                    <div className="row">
+                        <div className="col col-md-12 col-lg-6">
+                            <ItemList />
+                        </div>
+
+                        <div className="col col-md-12 col-lg-6">
+                            <PersonDetails />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default App
