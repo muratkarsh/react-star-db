5f8538f70a1f4b58383b2344ac486531986fafe7
murat <muratkarsh@gmail.com>
2019-03-11 14:17:12 +0200

    implement random-planet behavior logic

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './random-planet.scss'
+import SwapiService from '../../services/swapi-service.js'
 
-const RandomPlanet = () => {
-    return (
-        <div className='random-planet app__card'>
-            <img className='random-planet__img' src="https://starwars-visualguide.com/assets/img/planets/5.jpg" alt=""/>
-            <div className='random-planet__info'>
-                <h3>Planet name</h3>
+export default class RandomPlanet extends Component {
+    state = {
+        planet: {}
+    }
 
-                <ul>
-                    <li>Population: <span>102495</span></li>
-                    <li>Rotation period: <span>95</span></li>
-                    <li>Diametr: <span>100</span></li>
-                </ul>
+    swapiService = new SwapiService
+
+    updatePlanet = () => {
+        const id = Math.floor(Math.random() * (25 - 1)) + 1
+
+        this.swapiService.getPlanet(id)
+            .then(planet => this.setState({ planet }))
+    }
+
+    constructor(props) {
+        super(props);
+        this.updatePlanet()
+    }
+
+    render() {
+        const { planet: { id, name, population, rotation, diameter } } = this.state
+
+        return (
+            <div className='random-planet app__card'>
+                <img className='random-planet__img' src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt=""/>
+                <div className='random-planet__info'>
+                    <h3>{ name }</h3>
+
+                    <ul>
+                        <li>Population: <span>{ population }</span></li>
+                        <li>Rotation period: <span>{ rotation }</span></li>
+                        <li>Diameter: <span>{ diameter }</span></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default RandomPlanet
